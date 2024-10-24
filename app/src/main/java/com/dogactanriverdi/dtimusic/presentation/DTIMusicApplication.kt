c9ac55a697ebbdb631144b834a23f0312efcda89
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-10-24 16:42:57 +0300

    Complete hilt setup

---
+++ app/src/main/java/com/dogactanriverdi/dtimusic/presentation/DTIMusicApplication.kt
@@ -0,0 +1,7 @@
+package com.dogactanriverdi.dtimusic.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DTIMusicApplication : Application()
