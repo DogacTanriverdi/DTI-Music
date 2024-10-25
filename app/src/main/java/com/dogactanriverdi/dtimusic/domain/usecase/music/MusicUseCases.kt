5bbf7d3338a6041b635f936c7f27bf2a74603c39
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-10-25 11:50:44 +0300

    Add and provide get album from storage use case

--- app/src/main/java/com/dogactanriverdi/dtimusic/domain/usecase/music/MusicUseCases.kt
+++ app/src/main/java/com/dogactanriverdi/dtimusic/domain/usecase/music/MusicUseCases.kt
@@ -3,4 +3,5 @@ package com.dogactanriverdi.dtimusic.domain.usecase.music
 data class MusicUseCases(
     val getAllMusicFromStorage: GetAllMusicFromStorageUseCase,
     val getMusicByIdFromStorage: GetMusicByIdFromStorageUseCase,
+    val getAllAlbumFromStorage: GetAllAlbumFromStorageUseCase,
 )
