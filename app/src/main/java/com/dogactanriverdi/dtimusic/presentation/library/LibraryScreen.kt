633fc644edb9da1bedc16a93d8ce80a91c3c7393
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-10-27 17:34:58 +0300

    Delete playlist section in the library screen

--- app/src/main/java/com/dogactanriverdi/dtimusic/presentation/library/LibraryScreen.kt
+++ app/src/main/java/com/dogactanriverdi/dtimusic/presentation/library/LibraryScreen.kt
@@ -1,18 +1,12 @@
 package com.dogactanriverdi.dtimusic.presentation.library
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,12 +14,11 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.dogactanriverdi.dtimusic.presentation.library.LibraryContract.UiState
-import com.dogactanriverdi.dtimusic.presentation.library.LibraryContract.UiEffect
 import com.dogactanriverdi.dtimusic.presentation.library.LibraryContract.UiAction
+import com.dogactanriverdi.dtimusic.presentation.library.LibraryContract.UiEffect
+import com.dogactanriverdi.dtimusic.presentation.library.LibraryContract.UiState
 import com.dogactanriverdi.dtimusic.presentation.library.components.AlbumsTab
 import com.dogactanriverdi.dtimusic.presentation.main.MainContract
 import com.dogactanriverdi.dtimusic.presentation.main.MainViewModel
@@ -69,7 +62,7 @@ fun LibraryContent(
 
         var selectedTabIndex by remember { mutableIntStateOf(0) }
 
-        val tabTitles = listOf("Albums", "Playlists")
+        val tabTitles = listOf("Albums")
 
         Column(
             modifier = Modifier.fillMaxSize()
@@ -92,17 +85,12 @@ fun LibraryContent(
             }
 
             when (selectedTabIndex) {
-
                 0 -> {
                     AlbumsTab(albumList) { albumId ->
                         navController.navigate(
                             route = Screen.AlbumDetail.route + "?albumId=$albumId"
                         )
                     }
-                }
-
-                1 -> {
-                    Text(text = "Playlists")
                 }
             }
         }
